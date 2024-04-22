e49a0ee88b0b9f8d63f54b7db9f3de723be36c4c
Joel_Bruno_Ferreira <1191843@isep.ipp.pt>
2024-04-22 19:52:37 +0100

    Sprint B - Backoffice module added

---
+++ jobs4u.app.backoffice.console/src/main/java/backoffice/App_Backoffice.java
@@ -0,0 +1,12 @@
+package backoffice;
+
+public class App_Backoffice {
+    public static void main(String[] args) {
+        GreeterBackoffice greeter = new GreeterBackoffice();
+        System.out.println();
+        System.out.println("==============================");
+        System.out.println(greeter.sayHello());
+        System.out.println("==============================");
+        System.out.println();
+    }
+}
