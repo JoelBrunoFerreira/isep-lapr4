67c6cecba1387651d6342bff3f65bd9887cf038b
Victor Salgado <1221722@isep.ipp.pt>
2024-04-27 00:50:59 +0100

    CustomerManagerUI submenu's update

---
+++ jobs4u.app.backoffice.console/src/main/java/backoffice/presentation/registerjobopening/RegisterJobOpeningUI.java
@@ -0,0 +1,20 @@
+package backoffice.presentation.registerjobopening;
+
+import backoffice.presentation.AdminMainMenu;
+import backoffice.presentation.CustomerManagerMainMenu;
+import backoffice.presentation.OperatorMainMenu;
+import backoffice.presentation.backofficeUser.SignupRequestAction;
+import eapli.base.app.common.console.presentation.authz.LoginUI;
+import eapli.base.infrastructure.authz.AuthenticationCredentialHandler;
+import eapli.base.usermanagement.domain.BaseRoles;
+import eapli.framework.actions.ChainedAction;
+import eapli.framework.actions.menu.Menu;
+import eapli.framework.presentation.console.AbstractUI;
+import eapli.framework.presentation.console.ExitWithMessageAction;
+import eapli.framework.presentation.console.menu.MenuItemRenderer;
+import eapli.framework.presentation.console.menu.VerticalMenuRenderer;
+
+
+public class RegisterJobOpeningUI{
+
+}
