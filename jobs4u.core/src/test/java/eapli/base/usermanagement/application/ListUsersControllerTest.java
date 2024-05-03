d12b970dd2ee32003004df8fc1a914197de3cbe9
Joel_Bruno_Ferreira <1191843@isep.ipp.pt>
2024-05-03 22:05:11 +0100

    Sprint B - Small bug fix

--- jobs4u.core/src/test/java/eapli/base/usermanagement/application/ListUsersControllerTest.java
+++ jobs4u.core/src/test/java/eapli/base/usermanagement/application/ListUsersControllerTest.java
@@ -1,17 +1,11 @@
 package eapli.base.usermanagement.application;
 
-import eapli.base.candidate.domain.Candidate;
 import eapli.base.candidate.dto.CandidateDTO;
-import eapli.base.usermanagement.domain.BaseRoles;
-import eapli.framework.infrastructure.authz.application.AuthorizationService;
-import eapli.framework.infrastructure.authz.application.AuthzRegistry;
-import eapli.framework.infrastructure.authz.domain.model.Role;
 import org.junit.jupiter.api.Test;
 
 import java.util.Optional;
-import java.util.Set;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 class ListUsersControllerTest {
 
@@ -25,6 +19,6 @@ class ListUsersControllerTest {
         assertNotNull(candidate.get());
         assertNotNull(candidate.get().getEmail());
         assertNotNull(candidate.get().getPhoneNumber());
-        assertNotNull(candidate.get().getName());
+        assertNotNull(candidate.get().getFirstName());
     }
 }
