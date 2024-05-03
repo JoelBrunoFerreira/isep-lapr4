cec332eb81e82196724f54f79e022cf75bb8d6c1
JorgeCruz1221715 <1221715@isep.ipp.pt>
2024-05-03 21:56:53 +0100

    USs 1006 and 1001 - Unitary tests update

---
+++ jobs4u.core/src/test/java/eapli/base/customer/domain/CustomerTest.java
@@ -0,0 +1,52 @@
+package eapli.base.customer.domain;
+
+import eapli.base.usermanagement.application.RegisterUserService;
+import eapli.base.usermanagement.domain.BaseRoles;
+import eapli.framework.infrastructure.authz.domain.model.SystemUser;
+import org.junit.jupiter.api.Test;
+
+import java.util.Set;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class CustomerTest {
+
+    @Test
+    public void ensureCustomerUserHasName(){
+
+        RegisterUserService registerUserService = new RegisterUserService();
+        registerUserService.registerUser("USERNAME", "FIRSTNAME", "LASTNAME",
+                "EMAIL", Set.of(BaseRoles.CUSTOMER_USER));
+        SystemUser user = registerUserService.getRegisteredSystemUser();
+
+        Customer customer = new Customer(user, "ADDRESS");
+
+        assertEquals(customer.getName().toString(), "FIRSTNAME");
+
+    }
+
+    @Test
+    public void ensureCustomerUserHasEmail(){
+
+        RegisterUserService registerUserService = new RegisterUserService();
+        registerUserService.registerUser("USERNAME", "FIRSTNAME", "LASTNAME",
+                "EMAIL", Set.of(BaseRoles.CUSTOMER_USER));
+        SystemUser user = registerUserService.getRegisteredSystemUser();
+
+        Customer customer = new Customer(user, "ADDRESS");
+
+        assertEquals(customer.getName().toString(), "EMAIL");
+
+    }
+
+        @Test
+        public void ensureSystemUserIsCreated(){
+            RegisterUserService registerUserService = new RegisterUserService();
+            registerUserService.registerUser("USERNAME", "FIRSTNAME", "LASTNAME",
+                    "EMAIL", Set.of(BaseRoles.CUSTOMER_USER));
+            SystemUser user = registerUserService.getRegisteredSystemUser();
+
+            assertNotNull(user);
+        }
+
+}
