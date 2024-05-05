5f1479404f53846dfe11c8ad7d685fc874d1b438
Fabio Borges <1100719@isep.ipp.pt>
2024-05-05 20:04:00 +0100

    Sprint B - UC1007 test phaseDataVerification()

---
+++ jobs4u.app.backoffice.console/src/test/java/backoffice/presentation/CustomerManager/JobOpeningManagement/SetupRecruitmentPhasesUITest.java
@@ -0,0 +1,26 @@
+package backoffice.presentation.CustomerManager.JobOpeningManagement;
+
+import org.junit.jupiter.api.Test;
+
+import java.time.LocalDate;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class SetupRecruitmentPhasesUITest {
+
+//    SetupRecruitmentPhasesUI setupRecruitmentPhasesUI = new SetupRecruitmentPhasesUI();
+//
+
+//    @Test
+//    void phaseDateVerification(){
+//
+//
+//        LocalDate startDate = LocalDate.now();
+//        LocalDate endDate = LocalDate.now().plusDays(1);
+//        LocalDate maxDate = endDate;
+//        final boolean expected = setupRecruitmentPhasesUI.phaseDateVerification(startDate,endDate,maxDate);
+//
+//        assertTrue(expected);
+//
+//    }
+}
