b43d220b296b9ad8e60986b43ae991ec8310de88
Joel_Bruno_Ferreira <1191843@isep.ipp.pt>
2024-05-05 22:18:16 +0100

    Merge fabio branch into testing

--- jobs4u.app.backoffice.console/src/main/java/backoffice/presentation/operator/OperatorMainMenu.java
+++ jobs4u.app.backoffice.console/src/main/java/backoffice/presentation/operator/OperatorMainMenu.java
@@ -2,14 +2,12 @@ package backoffice.presentation.operator;
 
 import backoffice.Jobs4uBackofficeApp;
 import backoffice.presentation.operator.jobapplicationmanagement.RegisterJobApplicationUI;
-import eapli.base.usermanagement.application.ListUsersController;
 
 import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public class OperatorMainMenu {
     static Scanner read = new Scanner(System.in);
-    ListUsersController listUsersController = new ListUsersController();
     ListCandidateUI listCandidateUI = new ListCandidateUI();
     AddCandidateUI addCandidateUI = new AddCandidateUI();
     RegisterJobApplicationUI registerJobApplicationUI = new RegisterJobApplicationUI();
@@ -18,68 +16,62 @@ public class OperatorMainMenu {
     private static final int OPTION_2 = 2;
     private static final int OPTION_3 = 3;
     private static final int LOGOUT = 4;
-
     public void displayOperatorMenu() {
 
         while (true) {
             System.out.println("""
-                    =====================================
-                    |      Jobs4u - Operator Menu       |
-                    =====================================
-                    1. Register new Candidate
-                    2. List all Candidates
-                    3. Register Job Application
-                    4. Logout
-                    0. Exit
-                    =====================================
-                                    
-                    """);
+                =====================================
+                |      Jobs4u - Operator Menu       |
+                =====================================
+                1. Register new Candidate
+                2. List all Candidates
+                3. Register Job Application
+                4. Logout
+                0. Exit
+                =====================================
+                
+                """);
+            System.out.println();
+
+
             int option = 0;
             boolean validInput = false;
 
-            try {
-                option = read.nextInt();
+            while (!validInput) {
+                System.out.print("Enter option number: ");
 
-                while (!validInput) {
-                    System.out.print("Enter option number: ");
+                try {
+                    option = read.nextInt();
 
-                    try {
-                        option = read.nextInt();
-
-                        if (option >= EXIT_OPTION && option <= LOGOUT) {
-                            validInput = true;
-                        } else {
-                            System.out.printf("Invalid option. Please enter a number between %d and %d \n", EXIT_OPTION, LOGOUT);
-                        }
-                    } catch (InputMismatchException e) {
-                        System.out.printf("Invalid input. Please enter a number between %d and %d \n", EXIT_OPTION, LOGOUT);
-                        read.nextLine();
+                    if (option >= EXIT_OPTION && option <= LOGOUT) {
+                        validInput = true;
+                    } else {
+                        System.out.printf("Invalid option. Please enter a number between %d and %d \n", EXIT_OPTION, LOGOUT);
                     }
-
-                    switch (option) {
-                        case OPTION_1:
-                            addCandidateUI.doShow();
-                            break;
-                        case OPTION_2:
-                            listCandidateUI.doShow();
-                            break;
-                        case OPTION_3:
-                            registerJobApplicationUI.doShow();
-                            break;
-                        case LOGOUT:
-                            Jobs4uBackofficeApp.main(null);
-                            break;
-                        case EXIT_OPTION:
-                            System.out.println("Bye, Bye");
-                            System.exit(0);
-                            break;
-                        default:
-                            System.out.printf("Invalid input. Please enter a number between %d and %d.\n", EXIT_OPTION, LOGOUT);
-                    }
+                } catch (InputMismatchException e) {
+                    System.out.printf("Invalid input. Please enter a number between %d and %d \n", EXIT_OPTION, LOGOUT);
+                    read.nextLine();
                 }
-            } catch (InputMismatchException e) {
-                System.out.printf("Invalid input. Please enter a number between %d and %d \n", EXIT_OPTION, LOGOUT);
-                read.nextLine();
+            }
+            switch (option) {
+                case OPTION_1:
+                    addCandidateUI.doShow();
+                    break;
+                case OPTION_2:
+                    listCandidateUI.doShow();
+                    break;
+                case OPTION_3:
+                    registerJobApplicationUI.doShow();
+                    break;
+                case LOGOUT:
+                    Jobs4uBackofficeApp.main(null);
+                    break;
+                case EXIT_OPTION:
+                    System.out.println("Bye, Bye");
+                    System.exit(0);
+                    break;
+                default:
+                    System.out.printf("Invalid input. Please enter a number between %d and %d.\n", EXIT_OPTION, LOGOUT);
             }
         }
     }
--- jobs4u.app.backoffice.console/src/test/java/backoffice/presentation/CustomerManager/JobOpeningManagement/SetupRecruitmentPhasesUITest.java
+++ jobs4u.app.backoffice.console/src/test/java/backoffice/presentation/CustomerManager/JobOpeningManagement/SetupRecruitmentPhasesUITest.java
@@ -1,16 +1,5 @@
 package backoffice.presentation.CustomerManager.JobOpeningManagement;
 
-import eapli.base.RecruitmentProcessManagement.dto.RecruitmentProcessPhaseDTO;
-import org.junit.jupiter.api.Test;
-
-import java.time.LocalDate;
-import java.util.Collection;
-import java.util.Iterator;
-import java.util.List;
-import java.util.ListIterator;
-
-import static org.junit.jupiter.api.Assertions.*;
-
 class SetupRecruitmentPhasesUITest {
 
 //TODO error: java.lang.IllegalStateException
