73a8a0e60a845453260e3bf62df430254117dd66
Joel_Bruno_Ferreira <1191843@isep.ipp.pt>
2024-05-22 02:56:22 +0100

    Sprint C - SMTP Implementation

--- jobs4u.app.backoffice.console/src/main/java/backoffice/presentation/operator/AddCandidateUI.java
+++ jobs4u.app.backoffice.console/src/main/java/backoffice/presentation/operator/AddCandidateUI.java
@@ -5,11 +5,13 @@ import eapli.framework.domain.repositories.IntegrityViolationException;
 import eapli.framework.io.util.Console;
 import eapli.framework.presentation.console.AbstractUI;
 import eapli.framework.representations.dto.GeneralDTO;
+import mailClient.EmailService;
 import org.hibernate.exception.ConstraintViolationException;
 
 @SuppressWarnings("java:S106")
 public class AddCandidateUI extends AbstractUI {
     private final AddCandidateController controller = new AddCandidateController();
+    private final EmailService emailService = new EmailService();
 
     @Override
     public boolean doShow() {
@@ -26,14 +28,20 @@ public class AddCandidateUI extends AbstractUI {
             GeneralDTO registeredUser = controller.addCandidate(firstName, lastName, email, phoneNumber);
             System.out.println();
             System.out.println("================================");
-            System.out.println("Customer registered successfully");
+            System.out.println("Candidate registered successfully");
             System.out.println("First Name: " + registeredUser.getOrDefault("firstName", firstName));
+            String fName = registeredUser.getOrDefault("firstName", firstName).toString();
             System.out.println("Last Name: " + registeredUser.getOrDefault("lastName", lastName));
+            String lName = registeredUser.getOrDefault("lastName", lastName).toString();
             System.out.println("Email: " + registeredUser.getOrDefault("email", email));
+            String username = registeredUser.getOrDefault("email", email).toString();
             System.out.println("Phone Number: " + registeredUser.getOrDefault("phoneNumber", phoneNumber));
             System.out.println("Password: " + registeredUser.get("password"));
+            String userPassword = registeredUser.get("password").toString();
             System.out.println("Role(s): " + registeredUser.get("roles").toString());
             System.out.println("================================");
+            String emailBody = String.valueOf(buildEmail(fName, lName, username, userPassword));
+            emailService.sendEmail(username, emailBody);
             System.out.println();
             reRunMenu();
         } catch (IntegrityViolationException | ConstraintViolationException e) {
@@ -41,7 +49,6 @@ public class AddCandidateUI extends AbstractUI {
         } catch (Exception e) {
             System.out.println("Error registering candidate." + "\n" + e.getMessage());
         }
-
         return false;
     }
 
@@ -69,5 +76,25 @@ public class AddCandidateUI extends AbstractUI {
             reRunMenu();
         }
 
+    }
+
+    public StringBuilder buildEmail(String firstName, String lastName, String username, String password) {
+        StringBuilder message = new StringBuilder();
+        message.append("==============================");
+        message.append(System.getProperty("line.separator"));
+        message.append("      Welcome to Jobs4u       ");
+        message.append(System.getProperty("line.separator"));
+        message.append("==============================");
+        message.append(System.getProperty("line.separator"));
+        message.append("Mr/Mrs " + firstName + " " + lastName + " thank you for registering our application.");
+        message.append(System.getProperty("line.separator"));
+        message.append("Your username is: " + username);
+        message.append(System.getProperty("line.separator"));
+        message.append("Your password is: " + password);
+        message.append(System.getProperty("line.separator"));
+        message.append("==============================");
+        message.append(System.getProperty("line.separator"));
+
+        return message;
     }
 }
---
+++ jobs4u.followUp.server/src/main/java/mailClient/EmailService.java
@@ -0,0 +1,60 @@
+package mailClient;
+
+import jakarta.mail.Authenticator;
+import jakarta.mail.Message;
+import jakarta.mail.MessagingException;
+import jakarta.mail.PasswordAuthentication;
+import jakarta.mail.Session;
+import jakarta.mail.Transport;
+import jakarta.mail.internet.InternetAddress;
+import jakarta.mail.internet.MimeMessage;
+import java.util.Properties;
+
+public class EmailService {
+
+    public EmailService() {
+        // Empty Cosntructor
+    }
+
+    public void sendEmail(String to, String emailBody) {
+        // To From --> Our Domain
+        final String from = "sys-admin@jobs4u-isep.xyz";
+
+        // API Settings
+        final String username = "api";
+        final String password = "27b4dfc3f6f8fccdfdab16e45fdd8450";
+        final String host = "live.smtp.mailtrap.io";
+
+        // Mailtrap SMTP server config
+        Properties props = new Properties();
+        props.put("mail.smtp.auth", "true");
+        props.put("mail.smtp.starttls.enable", "true");
+        props.put("mail.smtp.host", host);
+        props.put("mail.smtp.port", "587"); // --> Recommended port by host
+
+        // Authentication
+        Authenticator authenticator = new Authenticator() {
+            protected PasswordAuthentication getPasswordAuthentication() {
+                return new PasswordAuthentication(username, password);
+            }
+        };
+        // Email message
+        Session session = Session.getInstance(props, authenticator);
+        try {
+            Message message = new MimeMessage(session);
+            // set From
+            message.setFrom(new InternetAddress(from));
+            // set To
+            message.setRecipients(Message.RecipientType.TO, InternetAddress.parse(to));
+            // set email subject field
+            message.setSubject("Jobs4u!");
+            // set the content of the email message
+            message.setContent(emailBody, "text/html");
+            // send the email message
+            Transport.send(message);
+            System.out.println("Email Message Sent Successfully");
+        } catch (MessagingException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
