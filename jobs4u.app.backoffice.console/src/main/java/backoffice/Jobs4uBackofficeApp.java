26c5bc15369424962b19ec9d2e1204a38f425679
JorgeCruz1221715 <1221715@isep.ipp.pt>
2024-06-01 16:21:32 +0100

    US-4000 (SCOMP) - All logic is implemented. Missing UI integration to display the words with the candidate data.

--- jobs4u.app.backoffice.console/src/main/java/backoffice/Jobs4uBackofficeApp.java
+++ jobs4u.app.backoffice.console/src/main/java/backoffice/Jobs4uBackofficeApp.java
@@ -1,9 +1,11 @@
 package backoffice;
 
 import backoffice.presentation.MainMenu;
+import eapli.base.app.bootstrap.BaseBootstrap;
 import eapli.base.app.common.console.BaseApplication;
 import eapli.base.app.common.console.presentation.authz.LoginUI;
 import eapli.base.infrastructure.authz.AuthenticationCredentialHandler;
+import eapli.base.infrastructure.bootstrapers.JobsDataBootstrap;
 import eapli.base.infrastructure.persistence.PersistenceContext;
 import eapli.base.usermanagement.domain.BasePasswordPolicy;
 import eapli.framework.infrastructure.authz.application.AuthzRegistry;
@@ -16,7 +18,7 @@ public class Jobs4uBackofficeApp extends BaseApplication {
     }
 
     public static void main(String[] args) {
-        //BaseBootstrap.main(args);
+        BaseBootstrap.main(args);
         //new JobsDataBootstrap().execute();
         new Jobs4uBackofficeApp().run(args);
     }
