5aa9192b739705144d5868b0e97e018939b9cba2
Joel_Bruno_Ferreira <1191843@isep.ipp.pt>
2024-06-09 22:10:18 +0100

    Sprint C - Final scripts update

--- build-all.sh
+++ build-all.sh
@@ -3,3 +3,7 @@
 # ECHO make sure JAVA_HOME is set to JDK folder
 # ECHO make sure maven is on the system PATH
 mvn -B $1 dependency:copy-dependencies package
+
+# Copy plugins JARs
+cd jobs4u.app.backoffice.console/target/dependency
+cp ../../../plugins/*.jar .
